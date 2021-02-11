7f9ac279fb43a9e02b71054f35a19698dde4fcae
vstrkv <37896140+vstrkv@users.noreply.github.com>
2021-02-12 04:32:23 +0700

    Add files via upload

---
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private UserDao userDaoImpl = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        userDaoImpl.createUsersTable();
+    }
+
+    public void dropUsersTable() {
+        userDaoImpl.dropUsersTable();
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        userDaoImpl.saveUser(name, lastName, age);
+    }
+
+    public void removeUserById(long id) {
+        userDaoImpl.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userDaoImpl.getAllUsers();
+    }
+
+    public void cleanUsersTable() {
+        userDaoImpl.cleanUsersTable();
+    }
+}
---
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,34 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    //CREATE SCHEMA `jpp_1_3` ;
+    private static final String URL = "jdbc:mysql://localhost:3306/jpp_1_3";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+
+    }
+
+    public static void shutdown() {
+        try {
+            getConnection().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
